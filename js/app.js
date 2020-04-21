fa6e750ece649bca04f8a6f78b35d56f9aef758e
Your Name <tt7.thiago@gmail.com>
2020-04-21 16:58:54 -0300

    Campo de detalhe da aplicação adicionado e algumas pequenas atualizações feitas no css

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const botao = document.querySelector('#cadastroAtividade');
 
 
 
-
  botao.addEventListener('click',()=>{
     let lista = document.querySelector('#atividades');
     let natividade = document.createElement('li');
